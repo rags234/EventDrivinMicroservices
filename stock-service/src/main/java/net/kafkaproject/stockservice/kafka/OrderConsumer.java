a5de3d630730e617fb568ab8db31a0e7e38e3171
Rajat <sagarmehtarajat136@gmail.com>
2024-01-23 10:08:29 +0530

    uploding base domains ,email-service,stock-service,order-service

---
+++ stock-service/src/main/java/net/kafkaproject/stockservice/kafka/OrderConsumer.java
@@ -0,0 +1,27 @@
+package net.kafkaproject.stockservice.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import net.kafkaproject.basedomains.dto.OrderEvent;
+
+//import net.kafkaproject.orderservice.kafka.OrderProducer;
+
+@Service
+public class OrderConsumer {
+
+	private static final Logger LOGGER=LoggerFactory.getLogger(OrderConsumer.class);
+	
+	@KafkaListener(
+		topics="${spring.kafka.topic.name}"
+		,groupId="${spring.kafka.consumer.group-id}"
+		)
+	public void consume(OrderEvent event) {
+		LOGGER.info(String.format("Order event received in stock service =>%s", event.toString()));
+	}
+	
+	
+	
+}
